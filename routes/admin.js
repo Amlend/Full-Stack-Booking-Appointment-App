479116c1ac8d598f1ced1a0db334a53f3e7d248a
Amlend <amlendsingh@gmail.com>
2024-03-30 22:16:10 +0530

    booking-app

---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const adminController = require("../controllers/booking");
+
+const router = express.Router();
+
+router.get("/", adminController.getAppointmentPage);
+
+router.post("/register", adminController.postRegister);
+router.get("/bookingSuccess", adminController.getBookingSuccess);
+
+router.get("/appointments", adminController.getAppointments);
+
+module.exports = router;
